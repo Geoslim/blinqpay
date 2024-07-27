224406b4d98b5d9adba80e55f1090ab3c0cc4d50
Geoslim <iedomwande@yahoo.com>
2024-07-27 05:42:11 +0100

    feat: add the routing logic to decide the best processor for each transaction

--- src/Services/PaymentProcessorRouter.php
+++ src/Services/PaymentProcessorRouter.php
@@ -2,7 +2,56 @@
 
 namespace Geoslim\Blinqpay\Services;
 
-class PaymentProcessorRouter
-{
+use Geoslim\Blinqpay\Contracts\BlinqpayPaymentRoutingInterface;
+use Geoslim\Blinqpay\Exceptions\BlinqpayException;
+use Geoslim\Blinqpay\Factories\PaymentProcessorAdapterFactory;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\App;
+use Exception;
 
+class PaymentProcessorRouter implements BlinqpayPaymentRoutingInterface
+{
+    protected PaymentProcessorService $paymentProcessorService;
+    protected PaymentProcessorAdapterFactory $paymentProcessorAdapterFactory;
+
+    public function __construct(
+        PaymentProcessorService $paymentProcessorService,
+        PaymentProcessorAdapterFactory $paymentProcessorAdapterFactory
+    )
+    {
+        $this->paymentProcessorService = $paymentProcessorService;
+        $this->paymentProcessorAdapterFactory = $paymentProcessorAdapterFactory;
+    }
+
+    /**
+     * @param array $transaction
+     * @return mixed
+     * @throws BlinqpayException
+     */
+    public function performTransaction(array $transaction)
+    {
+        try {
+            $processor = $this->paymentProcessorService->getPaymentProcessor($transaction);
+
+            if ($processor) {
+                $adapter = $this->paymentProcessorAdapterFactory->make($processor->name);
+
+                $result = $adapter->processPayment($transaction);
+                Log::info('Payment processed successfully', [
+                    'processor' => $processor->name,
+                    'transaction' => $transaction,
+                    'result' => $result,
+                ]);
+                return $result;
+            } else {
+                throw new BlinqpayException('No suitable payment processor found');
+            }
+        } catch (Exception $e) {
+            Log::error("Payment processing failed: " . $e->getMessage(), [
+                'transaction' => $transaction,
+                'exception' => $e
+            ]);
+            throw $e;
+        }
+    }
 }
--- src/Services/PaymentProcessorService.php
+++ src/Services/PaymentProcessorService.php
@@ -2,7 +2,93 @@
 
 namespace Geoslim\Blinqpay\Services;
 
+use Geoslim\Blinqpay\Exceptions\BlinqpayException;
+use Geoslim\Blinqpay\Models\BlinqpayPaymentProcessor;
+
 class PaymentProcessorService
 {
+    protected PaymentProcessorManager $paymentProcessorManager;
 
+    public function __construct(PaymentProcessorManager $paymentProcessorManager)
+    {
+        $this->paymentProcessorManager = $paymentProcessorManager;
+    }
+
+    /**
+     * @param array $transaction
+     * @return mixed|null
+     * @throws BlinqpayException
+     */
+    public function getPaymentProcessor(array $transaction)
+    {
+        $processors = $this->paymentProcessorManager->getActiveProcessors();
+        $supportedProcessors = $this->checkCurrencySupport($processors, $transaction['currency']);
+
+        return $this->decideBestPaymentProcessor($supportedProcessors, $transaction);
+    }
+
+    public function checkCurrencySupport($processors, $currency)
+    {
+        $supportedProcessors = [];
+
+        foreach($processors as $processor) {
+            if ($this->supportsCurrency($processor, $currency)) {
+                $supportedProcessors[] = $processor;
+            }
+        }
+
+        if (empty($supportedProcessors)) {
+            throw new BlinqpayException('No suitable payment processor found for currency: ' . $currency);
+        }
+
+        return $supportedProcessors;
+    }
+
+    /**
+     * @param BlinqpayPaymentProcessor $processor
+     * @param string $currency
+     * @return bool
+     */
+    protected function supportsCurrency(BlinqpayPaymentProcessor $processor, string $currency): bool
+    {
+        if (is_array($processor->supported_currencies)) {
+            return in_array($currency, $processor->supported_currencies);
+        } else {
+            return in_array($currency, explode(',' , $processor->supported_currencies));
+        }
+    }
+
+    protected function decideBestPaymentProcessor($supportedProcessors, $transaction)
+    {
+        $bestProcessor = null;
+        $bestScore = -INF;
+
+        foreach ($supportedProcessors as $processor) {
+            $score = $this->calculateScore($processor);
+            if ($score > $bestScore) {
+                $bestScore = $score;
+                $bestProcessor = $processor;
+            }
+        }
+
+        return $bestProcessor;
+    }
+
+    private function calculateScore($processor)
+    {
+        $rules = config('blinqpay.routing_rules');
+        $score = 0;
+
+        // Cost factor evaluation
+        if (isset($rules['transaction_fee'])) {
+            $score += $rules['transaction_fee'] * (1 / $processor->transaction_fee);
+        }
+
+        // Reliability factor evaluation
+        if (isset($rules['reliability'])) {
+            $score += $rules['reliability'] * $processor->reliability;
+        }
+
+        return $score;
+    }
 }
